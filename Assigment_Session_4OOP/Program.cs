cf0c8d5130f154e660cb032f1f4d5667cfa7c746
marwanmohameddd2006-ai <marwanmohameddd2006@gmail.com>
2026-08-16 11:36:34 +0300

    Add project files.

---
+++ Assigment_Session_4OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assigment_Session_4OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
