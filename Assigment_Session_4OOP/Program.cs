5a84777674b968bbfde586cff20773c8c94d6e48
marwanmohameddd2006-ai <marwanmohameddd2006@gmail.com>
2026-08-16 11:38:36 +0300

    Part 01-Q01

--- Assigment_Session_4OOP/Program.cs
+++ Assigment_Session_4OOP/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 — Theoretical Questions
+            #region Q01
+            //QA
+            //Q:  What is Abstraction in Object-Oriented Programming? 
+            /*Ans:
+             * is the process of hiding the implementation details 
+             * and showing only the essential features of an object to the user
+            */
+
+            //QB
+            //Q:  Why is abstraction considered one of the four pillars of OOP? 
+            /*Ans:
+             * Becaus with abstraction 
+                 * 1-Reduces complexity
+                 * 2-Improves maintainability
+                 * 3-Enhances security (hides sensitive details)
+                 * 4-Promotes flexibility and reusability
+             */
+            #endregion
+
+           
+
+            #endregion
         }
     }
 }
